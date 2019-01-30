6b481c1cf4b875d5c557a6d89f623f2d134bd6a7
nayamarosa <falecom@nayama.com.br>
2019-01-29 23:03:06 -0200

    implemento cipher.encode e cipher.decode, base de html e css(entradas de texto e botão)

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,5 @@
-function cipherStr(str, offPosition){
-    let strAscii = '';
-    for(let i = 0; i < str.length; i++){
-        if (str.charCodeAt(i) <= 90 && str.charCodeAt(i) >= 65){
-            let asciiPosition = ((str.charCodeAt(i) - 65 + offPosition) % 26) + 65;
-            strAscii += String.fromCharCode(asciiPosition);
-            console.log (strAscii)
-        } else if (str.charCodeAt(i) <= 122 && str.charCodeAt(i) >= 97){
-            let asciiPosition = ((str.charCodeAt(i) - 97 + offPosition) % 26) + 97;
-            strAscii += String.fromCharCode(asciiPosition);
-        }
-    }
-    return strAscii;
-}
-
-function decipherStr(str, offPosition){
-    let strAscii = '';
-    for(let i = 0; i < str.length; i++){
-        if (str.charCodeAt(i) <= 90 && str.charCodeAt(i) >= 65){
-            let asciiPosition = ((str.charCodeAt(i) - 65 - (offPosition % 26) + 26) % 26)  + 65;
-            strAscii += String.fromCharCode(asciiPosition);
-        } else if (str.charCodeAt(i) <= 122 && str.charCodeAt(i) >= 97){
-            let asciiPosition = ((str.charCodeAt(i) - 97 - (offPosition % 26) + 26 ) % 26) + 97;
-            strAscii += String.fromCharCode(asciiPosition);
-        }
-    }
-    return strAscii;
-}
-
-
-
-    function encode(str, offPosition){
+window.cipher = {
+    encode: function (offPosition, str){
         let strSeparet = str.split('');
         let strAscii = '';
         for(let i = 0; i < strSeparet.length; i++){
@@ -45,9 +15,9 @@ function decipherStr(str, offPosition){
             }
         }
         return strAscii;
-    }
+    },
     
-    function decode(str, offPosition){
+    decode: function (offPosition, str){
         let strSeparet = str.split('');
         let strAscii = '';
         for(let i = 0; i < strSeparet.length; i++){
@@ -65,3 +35,4 @@ function decipherStr(str, offPosition){
         return strAscii;
     }
      
+  };
